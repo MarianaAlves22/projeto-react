2494778fbe14986853621d8cf30c1d047f2365e5
Mariana Alves <Mary_A2903@hotmail.com>
2024-03-21 18:47:34 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Formulario from "./components/Formulario";
+import Perfil from "./components/Perfil";
+
+function App() {
+  return (
+    <>
+      <Perfil nome="Mariana" endereco="https://github.com/MarianaAlves22.png"/>
+      <Formulario/>
+    </>
+  )
+}
+
+export default App
